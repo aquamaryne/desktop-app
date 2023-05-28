2f11e82a1396439a073d05ac63c091859067bb17
n00k1e <plotnikoff1488@gmail.com>
2023-05-28 10:58:56 +0300

    many changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import entities from './db/typeorm'; 
+import entities from './db/typeorm';
 import { DbController } from './db/db.controller';
 import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: '',
+      host: '',
       port: 3306,
-      username: 'root',
-      password: 'toor',
-      database: 'projecttwo',
+      username: '',
+      password: '',
+      database: '',
       entities,
       synchronize: true,
     }),
