83ecd626b46ff107a5313b70c96c916f7a730d7a
n00k1e <plotnikoff1488@gmail.com>
2023-05-30 20:03:18 +0300

    many changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,12 +6,12 @@ import { DatabasesController } from './database/databases.controller';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: '',
+      host: '',
       port: 3306,
-      username: 'root',
-      password: 'toor',
-      database: 'projecttwo',
+      username: '',
+      password: '',
+      database: '',
       entities: [],
       synchronize: true,
     }),
