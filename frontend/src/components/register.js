5c8ee98f78a150c6f13ab1eb5785ef22b1d28a83
n00k1e <plotnikoff1488@gmail.com>
2023-06-03 00:11:57 +0300

    added some design for register form

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "../css/register.css";
 
 export default function RegisterForm(){
     const[formData, setFormData] = useState({
@@ -34,19 +35,26 @@ export default function RegisterForm(){
 
     return(
         <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor='name'>Name: </label>
-                <input type='text' id="name" name='name' value={formData.name} onChange={handleChange}/>
+            <label id='deadnet'> DeadNET </label>
+            <div className="form-item">
+                <label htmlFor='name'>Name</label>
+                <div className='input-wrapper'>
+                    <input type='text' id="name" name='name' value={formData.name} onChange={handleChange}/>
+                </div>
             </div>
-            <div>
-                <label htmlFor='email'>Email: </label>
-                <input type='email' id="email" name='email' value={formData.email} onChange={handleChange}/>
+            <div className="form-item">
+                <label htmlFor='email'>Email</label>
+                <div className='input-wrapper'>
+                    <input type='email' id="email" name='email' value={formData.email} onChange={handleChange}/>
+                </div>
             </div>
-            <div>
-                <label htmlFor='password'>Password: </label>
-                <input type='password' id="password" name='password' value={formData.password} onChange={handleChange}/>
+            <div className='input-wrapper'>
+                <label htmlFor='password'>Password</label>
+                <div className='input-wrapper'>
+                    <input type='password' id="password" name='password' value={formData.password} onChange={handleChange}/>
+                </div>
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" className='glitch-button'>Submit</button>
         </form>
     )
 }
