7e2c0b81b3aa0cbe356ce1e3aef98e320061e2fe
n00k1e <plotnikoff1488@gmail.com>
2023-06-25 14:14:10 +0300

    update some files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbConnect } from './database/index';
 import { UserRepositoryModule } from './user/user.module';
 
 
 @Module({
   imports: [ DbConnect, UserRepositoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
