d0cb49badf7b009f550a786910af793a28b8f7fa
n00k1e <plotnikoff1488@gmail.com>
2023-07-03 13:43:44 +0300

    some changes

--- frontend/src/components/mainpage.js
+++ frontend/src/components/mainpage.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from './search';
+import { Card } from '@mui/material';
+import  axios from 'axios';
 
 export default function MainPage(){
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localost:3001/users')
+            .then((res) => setUsers(res.data))
+            .catch((error) => console.error(error))
+    },[]);
+
     return(
         <div>
             <SearchBar />
+            {users.map((user) =>  (
+                <li>{user.name}</li>
+            ))}
         </div>
     )
 }
